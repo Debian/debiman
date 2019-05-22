3d27f575ad5faa0a823b8048d0cc355666b2a629
Kebert Xela <kebertxela86@gmail.com>
2019-05-22 10:11:39 -0700

    Various acronym lints (#112)

--- cmd/debiman/render_test.go
+++ cmd/debiman/render_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestBreadcrumbsToJSON(t *testing.T) {
-	const breadcrumbsJson = `{"@context":"http://schema.org","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"item":{"@type":"Thing","@id":"/contents-jessie.html","name":"jessie"}},{"@type":"ListItem","position":2,"item":{"@type":"Thing","@id":"/jessie/i3-wm/index.html","name":"i3-wm"}},{"@type":"ListItem","position":3,"item":{"@type":"Thing","@id":"","name":"i3(1)"}}]}`
+	const breadcrumbsJSON = `{"@context":"http://schema.org","@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"item":{"@type":"Thing","@id":"/contents-jessie.html","name":"jessie"}},{"@type":"ListItem","position":2,"item":{"@type":"Thing","@id":"/jessie/i3-wm/index.html","name":"i3-wm"}},{"@type":"ListItem","position":3,"item":{"@type":"Thing","@id":"","name":"i3(1)"}}]}`
 
 	const Suite = "jessie"
 	const Binarypkg = "i3-wm"
@@ -15,7 +15,7 @@ func TestBreadcrumbsToJSON(t *testing.T) {
 		{fmt.Sprintf("/%s/%s/index.html", Suite, Binarypkg), Binarypkg},
 		{"", "i3(1)"},
 	}
-	if got, want := string(b.ToJSON()), breadcrumbsJson; got != want {
+	if got, want := string(b.ToJSON()), breadcrumbsJSON; got != want {
 		fmt.Printf("%s\n", got)
 		t.Fatalf("unexpected breadcrumbs JSON: got %q, want %q", got, want)
 	}
